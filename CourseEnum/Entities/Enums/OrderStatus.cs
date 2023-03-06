5389599619e4b69f01fed17c7746446452d939c7
Inglide Noberto <inglidenoberto961@gmail.com>
2023-03-06 14:02:06 -0300

    Adicionar arquivos de projeto.

---
+++ CourseEnum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CourseEnum.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayments = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ CourseEnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using CourseEnum.Entities;
+using CourseEnum.Entities.Enums;
+
+namespace CourseEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayments
+            };
+
+            Console.WriteLine( order);
+        }
+    }
+}
